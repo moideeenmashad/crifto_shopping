10eff69fb726ea83deac85be2b19ae8aa2750297
Moideen Mashad <moideenmashad@gmail.com>
2023-11-10 21:44:22 +0530

    Created Navbar compononents

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Featured = () => {
+  return (
+    <div>
+        <h1>fea</h1>
+    </div>
+  )
+}
+
+export default Featured
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Featured from './Featured/Featured'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Featured/>
+    </div>
+  )
+}
+
+export default Home
