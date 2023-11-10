1a7f1c3d3463a6e4f868e806ae592caf68318bbd
Moideen Mashad <moideenmashad@gmail.com>
2023-11-10 22:01:35 +0530

    Created Navbar compononents

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+
+const Home = () => {
+  const [wish,setWish] = useState("Comeback after 30th Bro")
+  return (
+    <div>
+      <h1>{wish}</h1>
+    </div>
+  )
+}
+
+export default Home
