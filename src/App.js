9247e6922e678598e4ff0d704679b9239ac953fc
Moideen Mashad <moideenmashad@gmail.com>
2023-11-30 00:27:34 +0530

    Installed npm packages such as bootstrap,react-router,owl-carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
-import Home from './components/pages/Home';
-
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-   <Home/>
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 const Home = () => {
-  const [wish,setWish] = useState("Comeback after 30th Bro")
+  const [wish, setWish] = useState("Comeback after 30th Bro");
   return (
     <div>
       <h1>{wish}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
