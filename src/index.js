9934b66b9392f6d8cbf8e204d099b724b62afbff
Moideen Mashad <moideenmashad@gmail.com>
2024-01-01 16:30:13 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
