ec9d3bf96f99b38b7c9ed0bd3a694282bc800707
Moideen Mashad <moideenmashad@gmail.com>
2024-01-12 20:36:27 +0530

    Created Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,56 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-
-const Form = () => {
-    const [inputData, setInputData] = useState({
-        userName: String,
-        password: String,
-    })
-    const inputHandler = (event) => {
-        const { name, value } = event.target
-        setInputData(
-            (previousState) => ({
-                ...previousState,
-                [name]: value
-            })
-        )
-    }
-    // useForm()
-
-    const readValues = () => {
-        console.log(inputData)
-    }
-    return (
-        <div>
-
-            <div className="container">
-                <div className="row g-3">
-                    <div className="col-12">
-                        <h3>Sign In</h3>
-                        <label className="form-label" name="name">User Name</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            onChange={inputHandler}
-                            value={inputData.userName}
-                            name="userName" />
-                    </div>
-                    <div className="col-12">
-                        <label className="form-label" name="name">Password</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            onChange={inputHandler}
-                            value={inputData.password}
-                            name="password" />
-                    </div>
-                    <div className="col-12">
-                        <button className="btn btn-primary" onClick={readValues}>Sign in</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
 }
 
-export default Form
+export default Login
