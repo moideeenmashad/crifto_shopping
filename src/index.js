a03ffdb01b7416c783f67c103e3b8e2a7fc7d000
Moideen Mashad <moideenmashad@gmail.com>
2024-02-25 13:14:15 +0530

    Updated Folder Structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./Routes/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
