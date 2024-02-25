e9da2a472efc93e6909a7ef90a6ae128b452ee0a
Moideen Mashad <moideenmashad@gmail.com>
2024-02-25 12:13:23 +0530

    Updated

--- Backend/Models/Users.js
+++
@@ -1,9 +0,0 @@
-const Mongoose = require("mongoose");
-
-const usersSchema = Mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-const UserModal = Mongoose.model("Users", usersSchema);
-module.exports = UserModal;
